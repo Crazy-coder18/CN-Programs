22d79e12c961fa60acae07add53a8d3545bc7b5c
rapaka-usha <147684261+rapaka-usha@users.noreply.github.com>
2024-03-28 14:07:46 +0530

    Update program1b.c

--- program1b.c
+++ program1b.c
@@ -5,7 +5,7 @@ int main()
 {
     char a[100],b[100],c[100];
     int i,j=0,n,k=0;
-    printf("Enter the data");
+    printf("Enter the Data:");
     gets(a);
     n=strlen(a);
 
@@ -15,11 +15,11 @@ int main()
 
 
         if((a[i] == 'd' && a[i+1]=='l' && a[i+2]=='e')||
-(a[i] == 'e' && a[i+1]=='s' && a[i+2]=='c'))
+(a[i] == 'E' && a[i+1]=='S' && a[i+2]=='C'))
         {
-c[k++] = 'e';
-            c[k++] = 's';
-            c[k++] = 'c';
+            c[k++] = 'E';
+            c[k++] = 'S';
+            c[k++] = 'C';
 
         }
         c[k++] = a[i];
@@ -31,8 +31,8 @@ c[k++] = 'e';
 
     b[j++]='\0';
     printf("%s\n",b);
-    printf("DLESTX");
+    printf("DLESTX \t");
     printf("%s",c);
-     printf("DLEETX");
+     printf("\t DLEETX ");
     return 0;
 }
