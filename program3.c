28f1df6eaa2325c06b95097846289ece7578a6a2
Crazy-coder18 <162695626+Crazy-coder18@users.noreply.github.com>
2024-06-23 20:03:32 +0530

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,32 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
 
-int i,j,noframes,x,x1=10;
-for(i=0;i<200;i++)
-rand();
-noframes=rand()/200;
-i=1;
-j=1;
-noframes=noframes/8;
-printf("\n Number of frames is %d", noframes);
-getch();
-while(noframes>0)
-{
-printf("\n Sending frame %d",i);
-srand(x1++);
-x=rand()%10;
-if(x%2==0)
-{
-printf("\nsending frame %d",i);
-srand(x1++);
-x=rand()%10;
-}
-printf("\n Ack for frame %d ",j);
-noframes-=1;
-i++;
-j++;
-}
-printf("\n End of Stop and wait protocol");
+int main() {
+    int i, j, noframes, x, x1 = 10;
+
+    // Initialize random number generator
+    for (i = 0; i < 200; i++) {
+        rand();
+    }
+
+    // Determine the number of frames
+    noframes = rand() / 200;
+    noframes = noframes / 8;
+
+    printf("\nNumber of frames is %d", noframes);
+    getch();
+
+    i = 1;
+    j = 1;
+
+    while (noframes > 0) {
+        printf("\nSending frame %d", i);
+
+        srand(x1++);
+        x = rand() % 10;
+
+        if (x % 2 == 0) {
+            printf("\nSending frame %d", i);
+
+            srand(x1++);
+            x = rand() % 10;
+        }
+
+        printf("\nAck for frame %d", j);
+
+        noframes -= 1;
+        i++;
+        j++;
+    }
+
+    printf("\nEnd of Stop and Wait protocol");
+
+    return 0;
 }
